f566e75cb00adcc7b1905337f08d6b397ad804f5
DeepBlue-dot <deepblue5991@gmail.com>
2023-03-13 21:25:34 +0300

    DeepBlue contributions

--- Mastwal-Mesfin/BMI-Calac/BMI-Calac-Test.cpp
+++ Mastwal-Mesfin/BMI-Calac/BMI-Calac-Test.cpp
@@ -8,56 +8,94 @@ int main ()
   int B;
   cout<<"The number of people: ";
   cin>>B;
-  {
+  float arr[B][5];
+  
     for(I=0 ; I<B ; I++ )
     {
-    cout<<"Select Gender: 1.Male 2.Female\n"<<"->";
-    cin>>Gen;
-    cout<<"Enter Height: ";
-    cin>>h;
-    cout<<"Enter Mass: ";
-    cin>>w;
-    float Bmi;
-    Bmi = w/(h*h);
-    cout<<Bmi<<"\n";
-    switch(Gen)
-    {
-      case 1:
-    if (Bmi<=20)
-    {
-      cout<<"Under Weight\n";
-    }
-    else if (Bmi>20 && Bmi<25)
-    {
-      cout<<"Healthy\n";
-    }
-    else if (Bmi>=25)
-    {
-      cout<<"Over Weight\n";
-    }
-    else
-    {
-      cout<<"Select the appropriate numb\n";
-    }
-    break;
-    case 2:
-    if (Bmi<=18)
-    {
-      cout<<"Under";
-    }
-    else if (Bmi>18 && Bmi<23)
-    {
-      cout<<"Healthy";
-    }
-    else if (Bmi>=23)
-    {
-      cout<<"Over";
-    }
-    else
-    {
-      cout<<"Select the appropriate number";
-    }
-    }
-    }
-  }
+      cout<<"Select Gender: 1.Male 2.Female\n"<<"->";
+      cin>>arr[I][0];
+      cout<<"Enter Height: ";
+      cin>>arr[I][1];
+      cout<<"Enter Mass: ";
+      cin>>arr[I][2];
+      arr[I][3] = arr[I][2]/(arr[I][1]*arr[I][1]);
+      if(arr[I][0] == 1)
+      {
+      if (arr[I][3]<=20)
+      {
+        arr[I][4] = 1;
+      }
+      else if (arr[I][3]>20 && arr[I][3]<25)
+      {
+        arr[I][4] = 2;
+      }
+      else if (arr[I][3]>=25)
+      {
+        arr[I][4] = 3;
+      }
+      else
+      {
+        cout<<"Select the appropriate numb\n";
+      }
+      }
+      else if(arr[I][0] == 2)
+      {
+      if (arr[I][3]<=18)
+      {
+        arr[I][4] = 1;
+      }
+      else if (arr[I][3]>18 && arr[I][3]<23)
+      {
+        arr[I][4] = 2;
+      }
+      else if (arr[I][3]>=23)
+      {
+        arr[I][4] = 3;
+      }
+      else
+      {
+        cout<<"Select the appropriate number";
+      }
+      }
+      }
+      for(int q =0; q < B; q++)
+      {
+        for (int p = 0; p < 5; p++)
+        {
+          if(p == 0)
+          {
+              if(arr[q][0] == 1)
+            {
+              cout << "Male" <<"\t";
+            }
+            else if(arr[q][0] == 2)
+            {
+              cout << "Female" <<"\t";
+            }
+          }
+          else if(p == 4)
+          {
+            if(arr[q][4] == 1)
+            {
+              cout << "under" <<"\t";
+            }
+            else if(arr[q][4] == 2)
+            {
+              cout << "helthy" <<"\t";
+            }
+            else if(arr[q][4] == 3)
+            {
+              cout << "over" <<"\t";
+            }
+          }
+          else
+          {
+            cout << arr[q][p] <<"\t";
+          }
+           
+        }
+        cout << endl;
+        
+      }
+  
 }
